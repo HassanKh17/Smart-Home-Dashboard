82b032514d2f6ecbe6836f163d0fe7d242852e65
HassanKh17 <124251566+HassanKh17@users.noreply.github.com>
2025-10-18 23:09:06 +0100

    Initial Setup

---
+++ SmartHomeDashboardP/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SmartHomeDashboardP;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ SmartHomeDashboardP/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace SmartHomeDashboardP;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
